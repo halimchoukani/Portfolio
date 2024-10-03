06cb7f5cb78e879a25b7d38acfd16fcc73cecca7
halim <halim.choukani@gmail.com>
2024-10-03 06:52:22 +0100

    Refactor SVG components in Hero.jsx

--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -3,13 +3,18 @@ import React, { useState, useEffect, useRef } from "react";
 import { Svg1 } from "./SVG/Svg1";
 import { Svg2 } from "./SVG/Svg2";
 import { Svg3 } from "./SVG/Svg3";
+import { Svg4 } from "./SVG/Svg4";
+import { Svg5 } from "./SVG/Svg5";
+import { Svg6 } from "./SVG/Svg6";
+import { Svg7 } from "./SVG/Svg7";
+import { Svg8 } from "./SVG/Svg8";
 
 function Hero() {
   const [svgComponent, setSvgComponent] = useState(null);
   const textRef = useRef(null);
 
   // Array of SVG components
-  const svgComponents = [<Svg1 key="1" />, <Svg2 key="2" />, <Svg3 key="3" />];
+  const svgComponents = [<Svg1 key="1" />, <Svg2 key="2" />, <Svg3 key="3" />,<Svg4 key="4" />, <Svg5 key="5" />, <Svg6 key="6" />,<Svg7 key="7" />, <Svg8 key="8" />];
 
   useEffect(() => {
     // Function to animate the paths of the SVG
@@ -23,6 +28,9 @@ function Hero() {
           opacity: 1,
           stagger: 0.1,
           ease: "power2.inOut",
+          onUpdate: () => {
+            gsap.to(textRef.current, { opacity: 1,delay:0.2 });
+          }
         }
       );
     };
@@ -46,7 +54,7 @@ function Hero() {
           setTimeout(() => {
             animatePaths(); // Animate after changing SVG
           }, 50); // Adjust timing if necessary
-          gsap.to(textRef.current, { opacity: 1, duration: 0.5 });
+          
         },
       });
     }, 5000);
@@ -57,13 +65,15 @@ function Hero() {
   return (
     <div
       id="home"
-      className="w-[90%] md:w-[80%] h-screen m-auto rounded-3xl flex flex-col justify-center items-center mt-2"
+      className="w-[90%] md:w-[80%] h-screen m-auto rounded-3xl flex flex-col justify-center items-center mt-2 gap-4"
     >
-      <div ref={textRef}> {svgComponent} </div>
-      <h2 className="text-[25px] text-white">My name is Halim</h2>
-      <h3 className="text-[20px] text-white font-light">
-        I’m a Full-Stack developer from Tunisia
-      </h3>
+      <div className="h-[20%]" ref={textRef}> {svgComponent} </div>
+      <div className="flex flex-col justify-center items-center gap-1">
+        <h2 className="text-[25px] text-white">My name is Halim</h2>
+        <h3 className="text-[20px] text-white font-light">
+          I’m a Full-Stack developer from Tunisia
+        </h3>
+      </div>
     </div>
   );
 }
